2ae928cd69854ddd266ac4b93368358e8e769392
lunaraura <stiffyeggs@gmail.com>
2023-06-11 02:43:33 -0500

    Update overlay.js
    2000 <= 200

--- overlay.js
+++ overlay.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     var overlay = document.querySelector(".overlay");
     setTimeout(function() {
       overlay.style.opacity = 0;
-    }, 200);
+    }, 2000);
 
     setTimeout(function() {
       overlay.classList.add("hidden");
